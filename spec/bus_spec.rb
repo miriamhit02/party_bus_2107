10e55837c9ac38145d664b79deea60ebd87b38cb
Miriam Aceves <miriammerari02@gmail.com>
2021-07-07 17:02:06 -0400

    Unskip tests

--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -1,27 +1,28 @@
+require 'pry'
 require 'rspec'
 require './lib/bus'
 
 describe Bus do
   context 'Attributes' do
-    xit 'is a bus' do
+    it 'is a bus' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus).to be_an_instance_of(Bus)
     end
 
-    xit 'has a name' do
+    it 'has a name' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.name).to eq("Mikes Awesome Bus")
     end
 
-    xit 'has a capacity' do
+    it 'has a capacity' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.capacity).to eq(4)
     end
 
-    xit 'has no passengers by default' do
+    it 'has no passengers by default' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.passengers).to eq([])
@@ -30,16 +31,17 @@ describe Bus do
 
   # Iteration 2
   context 'Passengers' do
-    xit 'returns a list of passengers' do
+    it 'returns a list of passengers' do
       bus = Bus.new('Mikes Awesome Bus', 4)
       bus.add_passenger('Mike')
       bus.add_passenger('Megan')
       bus.add_passenger('Tim')
 
       expect(bus.passengers).to eq(['Mike', 'Megan', 'Tim'])
+
     end
 
-    xit 'returns a list of uppercased names' do
+    it 'returns a list of uppercased names' do
       bus = Bus.new('Mikes Awesome Bus', 4)
       bus.add_passenger('Mike')
       bus.add_passenger('Megan')
