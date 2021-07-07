4fc65bf8812ef7150afea0ac65db93fdc0c5ada9
Miriam Aceves <miriammerari02@gmail.com>
2021-07-07 16:38:15 -0400

    Create add_passenger class

--- lib/bus.rb
+++ lib/bus.rb
@@ -1,8 +1,13 @@
 class Bus
-  attr_reader :name, :capacity
-  
+  attr_reader :name, :capacity, :passengers
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @passengers = []
+  end
+
+  def add_passenger(new_passenger)
+    @passengers << new_passenger
   end
 end
